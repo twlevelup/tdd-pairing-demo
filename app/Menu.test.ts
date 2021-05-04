017a65a9409869a7a12d184029bf9404c5e94fc7
Karen Garcia <karenkgs@gmail.com>
2021-05-04 16:48:03 +1000

    Fix requirements

--- app/Menu.test.ts
+++ app/Menu.test.ts
@@ -28,15 +28,17 @@ describe(Menu, () => {
       );
     });
 
-    it("should print the current balance when option 1 is selected", () => {
-      menu.menuFilterOption("1");
-
-      expect(linePrinter.getPrintedLine(0)).toEqual(
-        `Your current balance is: $${ACCOUNT_INITIAL_BALANCE}`
-      );
+    describe("when option 1 is selected", () => {
+      it("should print the current balance when option 1 is selected", () => {
+        menu.menuFilterOption("1");
+  
+        expect(linePrinter.getPrintedLine(0)).toEqual(
+          `Your current balance is: $${ACCOUNT_INITIAL_BALANCE}`
+        );
+      });
     });
 
-    describe("when option 2 is selected", () => {
+    describe("when option 2 is selected to withdraw valid amount", () => {
       beforeEach(() => {
         lineReader.setLineToRead("400");
         menu.menuFilterOption("2");
@@ -88,6 +90,10 @@ describe(Menu, () => {
           `Your new account balance is: $${ACCOUNT_INITIAL_BALANCE}`
         );
       });
+    });
+
+    describe("when option 3 is selected", () => {
+      //TODO: Implement deposit functionality
     });
   });
 
